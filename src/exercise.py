4f05985d1a11a0eea37225c87b149d7715a206d5
Scott Morgan <smorgan@bridgend.ac.uk>
2021-01-25 21:46:23 +0000

    Adds methods referenced in README

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
+def divide(a,b):
+    return a/b
+
+def summation(a,b):
+    return a+b
+
 def main():
     #write your code below this line
+    divide(3,4)
+    summation(4,5)
 
 if __name__ == '__main__':
     main()
